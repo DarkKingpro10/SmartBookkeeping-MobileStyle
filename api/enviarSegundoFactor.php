822a2abdf803e91f72cba8fe102bf4cce812526e
DarkKingpro10 <jesusgogeta@outlook.com>
2022-09-21 09:44:53 -0600

    Preparing for heroku

--- api/enviarSegundoFactor.php
+++ api/enviarSegundoFactor.php
@@ -178,7 +178,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'despcontable3@gmail.com';                     //SMTP username
-    $mail->Password   = 'nihtvfkguqdsstdi';                                      //SMTP password
+    $mail->Password   = 'epmwcvqlxmctvxzh';                                      //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
